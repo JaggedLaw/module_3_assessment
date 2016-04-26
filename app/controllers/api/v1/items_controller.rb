376ca05a7340bf1689011571b941c2a681ab0dfe
ZooBot Mars <sasa345@yahoo.com>
2016-04-26 09:49:57 -0600

    item controller api actions show and destroy

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,4 +5,12 @@ class Api::V1::ItemsController < ApplicationController
     respond_with Item.all
   end
 
+  def show
+    respond_with Item.find(params[:id])
+  end
+
+  def destroy
+    Items.find(params[:id]).destroy
+  end
+
 end
--- test/integration/api/v1/items_test.rb
+++ test/integration/api/v1/items_test.rb
@@ -9,13 +9,37 @@ class ItemsTest < ActionDispatch::IntegrationTest
     @item2 = Item.create(name: "Item2", description: "this is item twozie")
   end
 
-  def test_items_are_displayed_as_json
+  def test_items_index_is_displayed_as_json
     get "/api/v1/items"
 
     output = JSON.parse(response.body)
 
     assert_response 200
     assert_equal 2, output.length
+    item_first = output.first
+    item_last = output.last
+    assert_equal item1.name, "Item1"
+    assert_equal item2.name, "Item2"
+
+  end
+
+  def test_items_show_is_displayed_as_json
+    get "/api/v1/items/1"
+
+    output = JSON.parse(response.body)
+
+    assert_response 200
+    assert_equal 6, output.length
+    item_first = output.first
+    assert_equal item1.name, "Item1"
+
+  end
+
+  def test_delete_request_deletes_item
+    delete "/api/v1/items/1"
+
+    assert_response 204
+
   end
 
 end
